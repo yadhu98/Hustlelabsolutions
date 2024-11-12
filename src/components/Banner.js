7082ccc52988e0c6ab2d12927f50666cddd955ea
yadhudesigntech <yadhu.krishnan@designtech.com>
2024-11-12 09:55:59 +0530

    Initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const [isVisible, setIsVisible] = useState(true); // Track visibility state
+  const [animationClass, setAnimationClass] = useState(""); // Animation class state
+
+  const toRotate = [
+    "Web Development", 
+    "Videography & Photography", 
+    "Influencer Marketing", 
+    "Ad Campaign", 
+    "Content Creation", 
+    "Social Media Marketing"
+  ];
+  
+  const delay = 3000;  // Time between switching to the next item
+
+  useEffect(() => {
+    let interval;
+
+    // Start or stop animation based on visibility
+    if (isVisible) {
+      setAnimationClass("animate__animated animate__fadeIn");
+      interval = setInterval(() => {
+        setCurrentTextIndex(prevIndex => (prevIndex + 1) % toRotate.length);
+      }, delay);
+    } else {
+      // Reset text rotation index and animation class when not in view
+      setCurrentTextIndex(0);
+      setAnimationClass("");
+    }
+
+    return () => clearInterval(interval); // Clean up interval on component unmount or visibility change
+  }, [isVisible,toRotate.length]);
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7} className="text-content">
+            <TrackVisibility
+              partialVisibility
+              onChange={(inView) => setIsVisible(inView)} // Update visibility state directly
+            >
+              {() => (
+                <div className={animationClass}>
+                  <span className="tagline">Welcome to Hustle Lab</span>
+                  <h1>
+                    <span>At Hustle Labs we do</span>{" "}<br />
+                    <span className="txt-rotate">
+                      <span className="wrap">
+                        {toRotate[currentTextIndex]}
+                      </span>
+                    </span>
+                  </h1>
+                  <p>
+                    At Hustle Labs, we believe in keeping things simple: Less Meetings, More Results. That’s why we’ve crafted three powerful offers: 
+                    <span style={{ color: '#DE6EA' }}> Simple Strategy, Automated Growth, and Fast Content—built</span> 
+                    to streamline your marketing, maximize impact, and fuel your growth without the fuss.
+                  </p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5} className="image-content">
+            <TrackVisibility
+              partialVisibility
+              onChange={(inView) => setIsVisible(inView)} // Track image visibility as well
+            >
+              {() => (
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ src/components/Projects.js
@@ -0,0 +1,103 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/webdev.jpg"
+import projImg2 from "../assets/img/vandph.jpg";
+import projImg3 from "../assets/img/smm.jpg";
+import projImg4 from "../assets/img/influcer.jpg";
+import projImg5 from "../assets/img/content.jpg";
+import projImg6 from "../assets/img/adcam.jpg";
+
+
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Web Development",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Videography & Photography",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Social media Marketing",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Content Creation",
+      description: "Design & Development",
+      imgUrl: projImg5,
+    },
+    {
+      title: "Influencer Marketing",
+      description: "Design & Development",
+      imgUrl: projImg4,
+    },
+    {
+      title: "Ad Campaign",
+      description: "Design & Development",
+      imgUrl: projImg6,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Services</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                {/* <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first"> */}
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    {/* </Tab.Pane> */}
+                    {/* <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container> */}
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2} alt=""></img>
+    </section>
+  )
+}
